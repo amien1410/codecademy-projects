5e51ca637e17eac0b821d10aed8e012608a69c22
amien1410 <68118454+amien1410@users.noreply.github.com>
2022-09-12 08:57:21 +0800

    complete oauth2 app.js

--- oauth2/app.js
+++ oauth2/app.js
@@ -4,17 +4,45 @@ const OAuth2Server = require('oauth2-server');
 
 const app = express();
 
-// Create oauth instance here
-const oauth = new OAuth2Server({
-  model: require('./model.js'),
-  allowBearerTokensInQueryString: true,
-  accessTokenLifetime: 60 * 60
-})
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const PORT = 4001;
+const PORT = 4001
+
+const oauth = new OAuth2Server({
+  model: require("./model"),
+  allowBearerTokensInQueryString: true
+})
+
+const authenticateRequest = (req, res, next) => {
+
+  let request = new OAuth2Server.Request(req);
+  let response = new OAuth2Server.Response(res)
+
+  return oauth.authenticate(request, response)
+    .then((token)=>{
+      next()
+    })
+    .catch((err) => {
+      res.sendFile(path.join(__dirname, 'public/error.html'))
+    })
+}
+
+const obtainToken = (req, res) => {
+  let request = new OAuth2Server.Request(req);
+  let response = new OAuth2Server.Response(res);
+
+  return oauth.token(request, response)
+    .then((token) => {
+      res.json(token);
+    })
+    .catch((err) => {
+      res.json(err);
+    })
+}
+
+app.all('/auth', obtainToken);
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/home.html'));
@@ -23,8 +51,13 @@ app.get('/login', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/login.html'));
 })
 
-app.get('/secret', (req, res)=>{
-    res.send('Welcome to the secret area.');
+app.get('/public', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/allowed.html'));
+})
+
+app.get('/secret', authenticateRequest, (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public/private.html'));
 })
 
 app.listen(PORT, ()=>console.log(`Listening on port ${PORT}`));
+
